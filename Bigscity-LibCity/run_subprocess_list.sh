46a49f72f005507f813c4f29e57ef6d5c1f90091
Sumin Han <hsm6911@kaist.ac.kr>
2024-10-09 14:43:55 +0900

    slacktest

---
+++ Bigscity-LibCity/run_subprocess_list.sh
@@ -0,0 +1,104 @@
+python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOULN --livepop --stemb
+python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOULN --livepop 
+python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOULN --stemb
+python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOULN 
+python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOULN --livepop --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOULN --livepop 
+python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOULN --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOULN 
+python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOULN --livepop --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOULN --livepop 
+python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOULN --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOULN 
+python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOULN --livepop --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOULN --livepop 
+python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOULN --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOULN 
+python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOULN --livepop --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOULN --livepop 
+python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOULN --stemb
+python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOULN 
+
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOUL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOUL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOUL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset SEOUL 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOUL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOUL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOUL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset SEOUL 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOUL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOUL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOUL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset SEOUL 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOUL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOUL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOUL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset SEOUL 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOUL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOUL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOUL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset SEOUL 
+
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset GANGNAM --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset GANGNAM --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset GANGNAM --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset GANGNAM 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset GANGNAM --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset GANGNAM --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset GANGNAM --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset GANGNAM 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset GANGNAM --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset GANGNAM --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset GANGNAM --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset GANGNAM 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset GANGNAM --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset GANGNAM --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset GANGNAM --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset GANGNAM 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset GANGNAM --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset GANGNAM --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset GANGNAM --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset GANGNAM 
+
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset HONGIK --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset HONGIK --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset HONGIK --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset HONGIK 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset HONGIK --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset HONGIK --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset HONGIK --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset HONGIK 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset HONGIK --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset HONGIK --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset HONGIK --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset HONGIK 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset HONGIK --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset HONGIK --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset HONGIK --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset HONGIK 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset HONGIK --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset HONGIK --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset HONGIK --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset HONGIK 
+
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset JAMSIL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset JAMSIL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset JAMSIL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model GRU      --dataset JAMSIL 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset JAMSIL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset JAMSIL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset JAMSIL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKDCRNN  --dataset JAMSIL 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset JAMSIL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset JAMSIL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset JAMSIL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGTS    --dataset JAMSIL 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset JAMSIL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset JAMSIL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset JAMSIL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKGMAN   --dataset JAMSIL 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset JAMSIL --livepop --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset JAMSIL --livepop 
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset JAMSIL --stemb
+# python run_model_livepop.py --task traffic_state_pred --model WKSTTN   --dataset JAMSIL 
